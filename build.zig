f2c5de268f389f90f1dc32a96b7f5cc8e543af24
Samuel Hobl <samuel.hobl@gmail.com>
2024-12-30 11:20:53 +0100

    refactor to also have importable lib

--- build.zig
+++ build.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
 pub fn build(b: *std.Build) void {
 
     // Standard target options allows the person running `zig build` to choose
@@ -13,6 +16,24 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const mvzr = b.dependency("mvzr", .{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "fast-meta-tags",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.root_module.addImport("mvzr", mvzr.module("mvzr"));
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
     const exe = b.addExecutable(.{
         .name = "fast-meta-tags",
         .root_source_file = b.path("src/main.zig"),
@@ -20,22 +41,62 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const mvzr = b.dependency("mvzr", .{});
     exe.root_module.addImport("mvzr", mvzr.module("mvzr"));
 
-    const tests = b.addTest(.{
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib_unit_tests.root_module.addImport("mvzr", mvzr.module("mvzr"));
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const mvzrx = b.dependency("mvzr", .{});
-    tests.root_module.addImport("mvzr", mvzrx.module("mvzr"));
+    exe_unit_tests.root_module.addImport("mvzr", mvzr.module("mvzr"));
 
-    const test_cmd = b.addRunArtifact(tests);
-    test_cmd.step.dependOn(b.getInstallStep());
-    const test_step = b.step("test", "Run the tests");
-    test_step.dependOn(&test_cmd.step);
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    b.installArtifact(exe);
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
